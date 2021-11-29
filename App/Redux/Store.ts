c0c7919d3f8f9949f6b4e2e14600338274d67277
AshryA <ahmed.ashry@vodafone.com>
2021-11-29 16:13:06 +0200

    [master] install redux and creating the store

---
+++ App/Redux/Store.ts
@@ -0,0 +1,12 @@
+import { configureStore, combineReducers } from '@reduxjs/toolkit'
+
+const reducers = combineReducers({})
+
+const store = configureStore({
+  reducer: reducers
+})
+
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
+
+export default store
