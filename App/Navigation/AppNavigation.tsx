ef5213730bd88b6c33f5b70a724427d68669ed38
AshryA <ahmed.ashry@vodafone.com>
2021-11-29 16:23:43 +0200

    [master] install react navigation and create AppNavigation container

---
+++ App/Navigation/AppNavigation.tsx
@@ -0,0 +1,10 @@
+import { NavigationContainer } from '@react-navigation/native'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+const AppNavigation = () => {
+  return (
+    <NavigationContainer>{/* Rest of your app code */}</NavigationContainer>
+  )
+}
+
+export default AppNavigation
